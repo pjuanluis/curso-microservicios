75ab2866f5cf17960f719b8e8ee884717df0a5d1
pjuanluis <jlpr130893>
2020-10-23 23:00:12 -0500

    Se agrega dependencia de Spring Boot Admin

--- admin-service/src/main/java/academy/digitallab/store/admin/AdminServiceApplication.java
+++ admin-service/src/main/java/academy/digitallab/store/admin/AdminServiceApplication.java
@@ -1,9 +1,10 @@
 package academy.digitallab.store.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableAdminServer
 @SpringBootApplication
 public class AdminServiceApplication {
 
